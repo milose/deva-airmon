e74bd00d6e6f0a3e6218edd66bb42fe2a606c980
Milos Sakovic <m@deva.co>
2016-12-24 22:57:25 +0000

    Convert to mono.

--- src/convert.js
+++ src/convert.js
@@ -5,8 +5,7 @@ import {
 } from 'child_process'
 
 export default (input_file, output_file, config) => {
-
-  let cmd = `ffmpeg -nostats -loglevel 0 -y -i ${input_file} -ar 32000 -c:a libmp3lame -b:a 64k ${output_file}`
+  let cmd = `ffmpeg -nostats -loglevel 0 -y -i ${input_file} -joint_stereo 1 -ar 32000 -c:a libmp3lame -b:a 64k -ac 1 ${output_file}`
 
   if (config.LOG_VERBOSE) {
     console.log(cmd)
