33f5ea3e8d88a8c31023b0914ca1b9239bd68238
Milos Sakovic <m@deva.co>
2017-01-14 04:18:51 +0100

    Conform to JavaScript standard.

--- src/convert.js
+++ src/convert.js
@@ -1,16 +1,13 @@
 'use strict'
 
-import {
-  exec
-} from 'child_process'
+import { exec } from 'child_process'
 
-export default (input_file, output_file, config) => {
-  let cmd = `ffmpeg -nostats -loglevel 0 -y -i ${input_file} -joint_stereo 1 -ar 32000 -c:a libmp3lame -b:a 64k -ac 1 ${output_file}`
+export default (inputFile, outputFile, config) => {
+  let cmd = `ffmpeg -nostats -loglevel 0 -y -i ${inputFile} -joint_stereo 1 -ar 32000 -c:a libmp3lame -b:a 64k -ac 1 ${outputFile}`
 
   if (config.LOG_VERBOSE) {
     console.log(cmd)
   }
 
   return exec(cmd)
-
 }
